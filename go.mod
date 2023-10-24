2b4ca25e62c18b4b7705afa8fd29a2ebd3dd3c9c
Kenneth Martinez <kenneth.martinez@jamf.com>
2023-10-24 16:25:14 -0400

    Added dog module

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Ken501/puppy
 
 go 1.21.1
+
+require github.com/Ken501/dog v0.0.0-20231024194004-6228d7b137e6 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,17 @@
 package puppy
 
+import (
+	"github.com/Ken501/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
 }
